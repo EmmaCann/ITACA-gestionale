6ff7928e2806eac51fe07bfaf5b1f183d3472f89
EmmaCannavo <cnnmme02s46c351q@studium.unict.it>
2025-02-16 18:39:48 +0100

    inizializzazione progetto

---
+++ gestionale/resources/js/Pages/Home.jsx
@@ -0,0 +1,13 @@
+// import { Link } from '@inertiajs/react';
+import React from 'react';
+const Home = () => {
+    return (
+        <div>
+            <h1>Benvenuto in Laravel + Inertia.js + React!</h1>
+        </div>
+    );
+};
+
+export default Home;
+
+
