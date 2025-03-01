745a8694224511778237bd23b45f9b43ac358122
EmmaCannavo <cnnmme02s46c351q@studium.unict.it>
2025-03-01 12:45:46 +0100

    creazione folders e logoitaca.jsx

---
+++ gestionale/resources/js/components/molecules/atoms/logoItaca.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logoItaca from "../../../../../public/images/logo-itaca.png";
+
+
+export const LogoItaca = () => {
+    return(
+        <div className="box-logo">
+            <img src={logoItaca} alt="Logo Itaca" className="w-[260px] h-[120px]" />
+        </div>
+    );
+
+    
+}
