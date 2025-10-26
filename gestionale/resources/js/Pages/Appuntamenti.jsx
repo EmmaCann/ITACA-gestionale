b515858efd40c3f6b8fe11923ee43f6084fdbc0a
EmmaCannavo <cnnmme02s46c351q@studium.unict.it>
2025-10-26 20:22:28 +0100

    fix layout in pagine nuove

--- gestionale/resources/js/Pages/Appuntamenti.jsx
+++ gestionale/resources/js/Pages/Appuntamenti.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Home from "./Home";
 
 const Appuntamenti = () => {
+    return (
+        <Home>
+            <div className="p-8">
+                <h1 className="text-2xl font-semibold">Questa è la pagina Appuntamenti</h1>
+                <p className="text-gray-600 mt-2">Contenuto della pagina appuntamenti</p>
+            </div>
+        </Home>
+    );
+};
 
-    return(
-        <div>
-            <h1>questa è la pagina appuntamenti</h1>
-        </div>
-    )
-}
 export default Appuntamenti;
--- gestionale/resources/js/Pages/Pagamenti.jsx
+++ gestionale/resources/js/Pages/Pagamenti.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Home from "./Home";
 
 const Pagamenti = () => {
+    return (
+        <Home>
+            <div className="p-8">
+                <h1 className="text-2xl font-semibold">Questa è la pagina Pagamenti</h1>
+                <p className="text-gray-600 mt-2">Contenuto della pagina pagamenti</p>
+            </div>
+        </Home>
+    );
+};
 
-    return(
-        <div>
-            <h1>questa è la pagina pagamenti</h1>
-        </div>
-    )
-}
 export default Pagamenti;
