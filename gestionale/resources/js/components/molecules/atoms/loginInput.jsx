99e53613a26f789ef7e31d097c4a4fea7de04abf
EmmaCannavo <cnnmme02s46c351q@studium.unict.it>
2025-12-22 16:23:30 +0100

    fix submit login con enter

--- gestionale/resources/js/components/molecules/atoms/loginInput.jsx
+++ gestionale/resources/js/components/molecules/atoms/loginInput.jsx
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 import { TogglePasswordIcon } from "./TogglePasswordIcon";
 
-export const LoginInput = ({ inputType, placeholder, iconLeft, onTextChanged, name, value }) => {
+export const LoginInput = ({
+    inputType,
+    placeholder,
+    iconLeft,
+    onTextChanged,
+    name,
+    value,
+}) => {
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-    const inputTypeFinal = inputType === "password" && isPasswordVisible ? "text" : inputType;
+    const inputTypeFinal =
+        inputType === "password" && isPasswordVisible ? "text" : inputType;
 
     const handleChange = (event) => {
         if (onTextChanged) {
-            onTextChanged(event); 
+            onTextChanged(event);
         }
     };
 
     return (
         <div className="bg-white drop-shadow-md text-[#50534C] rounded-[16px] flex items-center w-full h-[48px] px-4">
             {iconLeft && (
-                <img src={iconLeft} alt="Icon Left" className="w-[32px] h-[32px] mr-2 mt-2" />
+                <img
+                    src={iconLeft}
+                    alt="Icon Left"
+                    className="w-[32px] h-[32px] mr-2 mt-2"
+                />
             )}
             <input
                 type={inputTypeFinal}
@@ -24,6 +36,12 @@ export const LoginInput = ({ inputType, placeholder, iconLeft, onTextChanged, na
                 name={name}
                 onChange={handleChange}
                 className="w-full text-[#50534C] text-[14px] text-start focus:outline-none px-4"
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        e.preventDefault();
+                        e.target.form?.requestSubmit();
+                    }
+                }}
             />
             {inputType === "password" && (
                 <TogglePasswordIcon
