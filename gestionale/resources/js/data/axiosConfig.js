978e1b14f313df0e3df87c7ecc70af122f71d9d5
EmmaCannavo <cnnmme02s46c351q@studium.unict.it>
2025-12-23 12:14:14 +0100

    Revert "modifiche axios"
    This reverts commit e0b4bfc24bca9763bbfa50656eb9a82746d8b914.

--- gestionale/resources/js/data/axiosConfig.js
+++ gestionale/resources/js/data/axiosConfig.js
@@ -1,13 +1,5 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL
-// });
-
 import axios from "axios";
 
-const baseURL = (import.meta.env.VITE_API_URL || "").trim();
-
-export const axiosInstance = axios.create(
-  baseURL ? { baseURL } : {}
-);
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+});
